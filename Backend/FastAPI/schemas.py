b215cbe823576b0b0daf28fc3fe8628033ea97ab
mrojas32 <131390769+mrojas32@users.noreply.github.com>
2024-05-26 18:53:00 -0400

    Update schemas.py

--- Backend/FastAPI/schemas.py
+++ Backend/FastAPI/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class UsuarioData(BaseModel):
     nombre: str
@@ -6,5 +7,30 @@ class UsuarioData(BaseModel):
     estudiante: bool
     correo: str
 
+class AdministradorData(BaseModel):
+    nombre: str
+    passwd: str
+
+class ReservaData(BaseModel):
+    fecha: datetime
+    bloq_ini: str
+    bloq_final: str
+    ID_usuario: int
+    ID_canchas: int
+
+class CanchasData(BaseModel):
+    nombre: str
+    tipo: str
+    ubicacion: str
+
+class AdministradorID(AdministradorData):
+    ID_admin: int
+
 class UsuarioID(UsuarioData):
     ID_usuario: int
+
+class ReservaID(ReservaData):
+    ID_reserva : int
+
+class CanchasID(CanchasData):
+    c : int
