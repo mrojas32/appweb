d8ef74a318330fc0077c9e85d5f370936c127d50
Cristóbal Díaz <cristobal.diazvi@usm.cl>
2024-05-26 04:25:19 -0400

    backen v1.4

--- Backend/FastAPI/crud_user.py
+++ Backend/FastAPI/crud_user.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Session
 
+from sqlalchemy import delete, update
+
 from models import Usuario
-from schemas import UsuarioData
+from schemas import UsuarioData, UsuarioID
 
 def get_users(db: Session):
     return db.query(Usuario).all()
@@ -13,8 +15,29 @@ def get_user_by_name(db: Session, nombre :str):
     return db.query(Usuario).filter(Usuario.nombre == nombre).first()
 
 def create_user(db: Session, user: UsuarioData):
-    new_user = Usuario(nombre = user.nombre, passwd = user.passwd)
+    new_user = Usuario(nombre = user.nombre, passwd = user.passwd, correo = user.correo, estudiante = user.estudiante)
     db.add(new_user)
     db.commit()
     db.flush(new_user)
     return new_user
+
+def get_user_by_name_passwd(db: Session, nombre :str, passwd: str):
+    return db.query(Usuario).filter(Usuario.nombre == nombre and Usuario.passwd == passwd).first()
+
+def delete_user_by_name(db: Session, user: Usuario):
+    db.delete(user)
+    db.commit()
+    db.flush()
+    return user
+
+def update_user_name(db: Session, user: Usuario, nombre:str):
+    db.execute(update(Usuario).where(Usuario.ID_usuario == user.ID_usuario).values(nombre = nombre))
+    db.commit()
+    db.flush()
+    return "Cambio realizado"
+
+def update_user_passwd(db: Session, user: Usuario, passwd:str):
+    db.execute(update(Usuario).where(Usuario.ID_usuario == user.ID_usuario).values(passwd = passwd))
+    db.commit()
+    db.flush()
+    return "Cambio realizado"
--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -23,18 +23,49 @@ def root():
 
 @app.get('/api/users', response_model = list[UsuarioID])
 def get_users(db: Session = Depends(get_db)):
-    return crud.get_users(db=db)
+    return crud_user.get_users(db=db)
 
 @app.get('/api/users/{id:int}', response_model=UsuarioID)
 def get_user(id, db: Session = Depends(get_db)):
-    user_by_id = crud.get_user_by_id(db=db, id=id)
+    user_by_id = crud_user.get_user_by_id(db=db, id=id)
     if user_by_id:
         return user_by_id
     raise HTTPException(status_code=404, detail='User not Found')
 
+@app.get('/api/users/{nombre:str}', response_model=UsuarioID)
+def get_user_by_name(nombre, db: Session = Depends(get_db)):
+    user_by_name = crud_user.get_user_by_name(db=db, nombre=nombre)
+    if user_by_name:
+        return user_by_name
+    raise HTTPException(status_code=404, detail='User not Found')
+
 @app.post('/api/users/', response_model=UsuarioID)
 def create_user(user: UsuarioData, db: Session = Depends(get_db)):
-    check_name = crud.get_user_by_name(db=db, name=user.name)
+    check_name = crud_user.get_user_by_name(db=db, nombre=user.nombre)
     if check_name:
         raise HTTPException(status_code=400, detail='User already exits')
-    return crud.create_user(db=db, user=user)
+    return crud_user.create_user(db=db, user=user)
+
+@app.delete('/api/delete_user/{nombre: str}', response_model=UsuarioID)
+def delete_user(nombre, db: Session = Depends(get_db)):
+    check_name = crud_user.get_user_by_name(db=db, nombre=nombre)
+    if check_name:
+        return crud_user.delete_user_by_name(db=db, user=check_name)
+    else:
+        raise HTTPException(status_code=400, detail='User dont exist')
+
+@app.put('/api/update_nom_user/{new_nombre:str , nombre:str}', response_model = str)
+def update_nom_user(new_nombre, nombre, db: Session = Depends(get_db)):
+    check_name = crud_user.get_user_by_name(db=db, nombre=nombre)
+    if check_name:
+        return crud_user.update_user_name(db=db, user=check_name,nombre=new_nombre)
+    else:
+        raise HTTPException(status_code=400, detail='User dont exist')
+
+@app.put('/api/update_nom_user/{new_passwd:str , nombre:str}', response_model = str)
+def update_nom_user(new_passwd, nombre, db: Session = Depends(get_db)):
+    check_name = crud_user.get_user_by_name(db=db, nombre=nombre)
+    if check_name:
+        return crud_user.update_user_passwd(db=db, user=check_name, passwd=new_passwd)
+    else:
+        raise HTTPException(status_code=400, detail='User dont exist')
