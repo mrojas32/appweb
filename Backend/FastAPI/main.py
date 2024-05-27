497b9fe66a96881bfc2517f47de938d011113005
Alexey Mitjaew <alexey.mitjaew@sansano.usm.cl>
2024-05-26 23:30:06 -0400

    backend fixes

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 import crud
@@ -9,6 +10,14 @@ from models import Base
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = localSession()
